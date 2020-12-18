08c65104291a251e2ff1c27e0436110941d67b8e
NolanWagoner <nwagone1@uncc.edu>
2020-12-17 23:01:05 -0500

    key now deleted when expired

--- personalBudgetFinal/src/app/auth.guard.ts
+++ personalBudgetFinal/src/app/auth.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -9,11 +10,17 @@ export class AuthGuard implements CanActivate {
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     //Returns true upon existence of access_token
     if (localStorage.getItem('access_token')) {
-      return true;
+      const helper = new JwtHelperService();
+      if(!helper.isTokenExpired(localStorage.getItem('access_token'))){
+        return true;
+      } else{
+        localStorage.clear();
+        window.alert('Login token has expired: you have been automatically logged out')
+      }
     }
 
     //Action for when user not verified
     this.router.navigate(['']);
-      return false;
+    return false;
   }
 }
